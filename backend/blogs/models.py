56d254bf95525dd90b4e2acbf36378fdc2e87b1e
Форов Александр <5h9t5@list.ru>
2024-02-10 18:59:26 +0300

    feat(blog/models): Создал модель Blog

---
+++ backend/blogs/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='blogs')
+    title = models.CharField('Название', max_length=100)
+    text = models.TextField('Текст')
+    created = models.DateTimeField('Дата создания', auto_now_add=True)
+    published = models.BooleanField(
+        'Опубликовано',
+        default=False,
+        help_text='Отметьте, чтобы опубликовать запись'
+    )
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.title
