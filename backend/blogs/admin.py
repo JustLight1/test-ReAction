c9a2a6be9ec6af7088c9d4b1bb7e2ab06a9cea99
Форов Александр <5h9t5@list.ru>
2024-02-11 00:11:58 +0300

    feat(blogs/admin): Добавлена возможность создавать, изменять, удалять посты

--- backend/blogs/admin.py
+++ backend/blogs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user', 'created', 'published')
+    list_filter = ('created', 'published')
+    search_fields = ('title', 'text')
+    ordering = ('-created',)
