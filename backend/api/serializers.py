84ac0500112d5e3c185f9cb301d9b809fa210620
Форов Александр <5h9t5@list.ru>
2024-02-11 00:07:17 +0300

    feat(api/serializers): Полю users присвоен SlugRelatedField

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
 from blogs.models import Blog
 
 
 class BlogSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
         model = Blog
         fields = ('id', 'user', 'title', 'text', 'created', 'published')
