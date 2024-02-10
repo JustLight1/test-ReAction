8cb8fdf7658ea9a865490a2b0af8cffca904e0f3
Форов Александр <5h9t5@list.ru>
2024-02-11 00:09:32 +0300

    feat(api/views): Добавлена логика для чтения, создания, изменения и удаления постов

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from blogs.models import Blog
+from .permissions import IsOwnerOrAdmin
+from .serializers import BlogSerializer
+
+
+class BlogListCreateAPIView(APIView):
+
+    def get(self, request):
+        posts = Blog.objects.all()
+        serializer = BlogSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogRetrieveUpdateDestroyAPIView(APIView):
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_object(self, pk):
+        try:
+            return Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = BlogSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        self.check_object_permissions(request, self.get_object(pk))
+        post = self.get_object(pk)
+        serializer = BlogSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        self.check_object_permissions(request, self.get_object(pk))
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
