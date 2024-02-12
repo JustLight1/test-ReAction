8a090b57ed92969ec6231cad2f449bae588750c0
Форов Александр <5h9t5@list.ru>
2024-02-12 14:59:50 +0300

    feat(api/views): В классе BlogRetrieveUpdateDestroyAPIView изменен метод get_object с помощью get_object_or_404

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,16 @@
-from django.http import Http404
 from rest_framework import status
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 from blogs.models import Blog
+
 from .permissions import IsOwnerOrAdmin
 from .serializers import BlogSerializer
 
 
 class BlogListCreateAPIView(APIView):
+    """Чтение и создание постов."""
 
     def get(self, request):
         posts = Blog.objects.all()
@@ -24,13 +26,12 @@ class BlogListCreateAPIView(APIView):
 
 
 class BlogRetrieveUpdateDestroyAPIView(APIView):
+    """Чтение, изменение и удаление постов по id."""
+
     permission_classes = [IsOwnerOrAdmin]
 
     def get_object(self, pk):
-        try:
-            return Blog.objects.get(pk=pk)
-        except Blog.DoesNotExist:
-            raise Http404
+        return get_object_or_404(Blog, pk=pk)
 
     def get(self, request, pk):
         post = self.get_object(pk)
@@ -43,7 +44,7 @@ class BlogRetrieveUpdateDestroyAPIView(APIView):
         serializer = BlogSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
