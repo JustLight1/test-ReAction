7840eebcaea3403a34956a0cf7a82fe552df96da
Форов Александр <5h9t5@list.ru>
2024-02-12 14:57:03 +0300

    style(api/urls): Изменено очередность  маршрутов

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
-from .views import (BlogListCreateAPIView,
-                    BlogRetrieveUpdateDestroyAPIView)
+
+from .views import BlogListCreateAPIView, BlogRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
     path('posts/', BlogListCreateAPIView.as_view()),
     path('posts/<int:pk>/', BlogRetrieveUpdateDestroyAPIView.as_view()),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
