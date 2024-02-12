a90b5fe5880904b16a47c29e372b48063aa91abd
Форов Александр <5h9t5@list.ru>
2024-02-12 14:54:43 +0300

    feat(api/permissions): Добавлено сообщение возникающее при исключении

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,12 +1,20 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 
 class IsOwnerOrAdmin(permissions.BasePermission):
+    """Проверка авторизации и доступа к объектам."""
+
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-            )
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user or request.user.is_staff
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if obj.user == request.user or request.user.is_staff:
+            return True
+        raise PermissionDenied('Вы не можете изменить или удалить этот пост,'
+                               'так как он принадлежит другому пользователю.')
